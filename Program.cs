188fd608fd773e0ed92f39bf617e6a58f7925a2f
Kartik Pande <73398686+S1XTUS@users.noreply.github.com>
2024-03-14 17:13:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloWorldDotNet6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world from .NET 6!");
+        }
+    }
+}
